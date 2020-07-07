10881bba4c1035545b3499380c16e4aba90d38b6
Victor <vgdobon91@gmail.com>
2020-07-07 11:00:35 -0400

    Escalera de numeros

--- src/EscaleraInvertida.java
+++ src/EscaleraInvertida.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
-public class EscaleraDeNumeros {
+public class EscaleraInvertida {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
         System.out.println("¿Cuantas peldaños quieres en tu escalera?");
         int peldaños=entrada.nextInt();
 
-
-        for (int i=peldaños;i>=1;i--){
-            for(int j=1;j<=i;j++){
+        for(int i=1;i<=peldaños;i++){
+            for(int j=1;j<=peldaños;j++){
                 System.out.print(j+" ");
-
             }
             System.out.println();
         }
+
     }
 }
